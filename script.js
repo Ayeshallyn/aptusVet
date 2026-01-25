c2a9b79bf909390854a1ec7938508e1f8fc37e27
Ayeshallyn <elagunero04@gmail.com>
2026-01-25 12:34:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-console.log("Aptus Vet Dashboard Loaded");
 
-// Example: simple alert for logout
-document.querySelector(".logout").addEventListener("click", () => {
-  alert("You have been logged out.");
-});
